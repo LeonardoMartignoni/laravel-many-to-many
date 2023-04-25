0aa7038252ad4304b3a6b30d97f694c8e89ce792
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-04-25 18:18:52 +0200

    Adjustments + added controller for Technology

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,12 +3,24 @@
 @section('page-title', $project->title)
 @section('content')
   <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Back to all projects</a>
-  <br>
-  {{-- @dump($project->type?->label) --}}
+  <hr>
+  <div class="row">
+    <div class="col-8">
+      <img src="{{ $project->getImageThumb() }}" alt="Project Thumbnail" width="240px" class="d-block my-4">
+      <p><strong>Description:</strong><br> {{ $project->description }}</p>
+    </div>
+    <div class="col-4 text-end">
+      <h3>Type:</h3>
+      <span class="badge rounded-pill"
+        style="background-color: {{ $project->type?->color ?? 'black' }}">{{ $project->type?->label ?? 'No type' }}</span>
 
-  <span class="badge rounded-pill"
-    style="background-color: {{ $project->type?->color ?? 'black' }}">{{ $project->type?->label ?? 'No type' }}</span>
-
-  <img src="{{ $project->getImageThumb() }}" alt="Project Thumbnail" width="240px" class="d-block my-4">
-  <p><strong>Description:</strong><br> {{ $project->description }}</p>
+      <h3 class="mt-5">Technologies:</h3>
+      @forelse ($project->technologies as $technology)
+        <span class="badge rounded-pill"
+          style="background-color: {{ $technology->color }}">{{ $technology->label }}</span>
+      @empty
+        Nessun label
+      @endforelse
+    </div>
+  </div>
 @endsection
